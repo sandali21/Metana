78e0c32fd4ad36c9038202ffbc9f21f5320e54bf
Sandali <sandaliherath00@gmail.com>
2025-03-10 21:08:54 +0530

    commit config and requirements files

---
+++ config.py
@@ -0,0 +1,9 @@
+AWS_ACCESS_KEY = "your-aws-access-key"
+AWS_SECRET_KEY = "your-aws-secret-key"
+S3_BUCKET_NAME = "your-s3-bucket-name"
+GOOGLE_SHEET_ID = "your-google-sheet-id"
+WEBHOOK_URL = "https://rnd-assignment.automations-3d6.workers.dev/"
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL_SENDER = "your-email@gmail.com"
+EMAIL_PASSWORD = "your-email-password"
